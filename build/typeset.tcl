8229d1892253101df4d4f403942043d512c32567
Lars Brinkhoff <lars@nocrew.org>
2021-08-27 13:09:54 +0200

    Typeset CLU papers. *IF MISSING FILES ADDED*

--- build/typeset.tcl
+++ build/typeset.tcl
@@ -18,3 +18,10 @@ expect ":KILL"
 # R manual.
 respond "*" ":cwd r\r"
 respond "*" ":r rman.r\r"
+
+# CLU Reference Manual.
+respond "*" ":cwd clu\r"
+respond "*" ":r refman\r"
+
+# Abstraction Mechanisms in CLU.
+respond "*" ":r clupap\r"
